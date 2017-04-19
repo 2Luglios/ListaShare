c9efdcb2acd42812f12d24037c016c2f7c537573
Ettore Luglio <ettoreluglio@Tamadrum.local>
2017-04-19 17:11:11 -0300

    minors

--- ListaShare/controllers/MeusDadosViewController.swift
+++ ListaShare/controllers/MeusDadosViewController.swift
@@ -15,4 +15,22 @@ class MeusDadosViewController : UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var senhaTextField: UITextField!
     @IBOutlet weak var repitaTextField: UITextField!
+    
+    var usuario = Usuario()
+    
+    override func viewDidLoad() {
+        usuario.load()
+        loginTextField.text = usuario.nome
+        emailTextField.text = usuario.email
+    }
+    
+    @IBAction func salvar(_ sender: UIBarButtonItem) {
+        usuario.nome = loginTextField.text
+        usuario.email = emailTextField.text
+        
+        usuario.save()
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
 }
--- ListaShare/modelo/Usuario.swift
+++ ListaShare/modelo/Usuario.swift
@@ -15,6 +15,14 @@ class Usuario: NSObject, NSCoding {
     var email:String!
     var senha:String!
     
+    override init() {
+        super.init()
+        
+        nome = ""
+        email = ""
+        senha = ""
+    }
+    
     func encode(with aCoder: NSCoder) {
         aCoder.encode(nome, forKey:"nome")
         aCoder.encode(email, forKey:"email")
@@ -24,7 +32,7 @@ class Usuario: NSObject, NSCoding {
     required init?(coder aDecoder: NSCoder) {
         nome = aDecoder.decodeObject(forKey:"nome") as! String
         email = aDecoder.decodeObject(forKey:"email") as! String
-        senha = aDecoder.decodeObject(forKey:"senha") as! String
+//        senha = aDecoder.decodeObject(forKey:"senha") as! String
     }
     
     func save () {
